c14dbaa2a2d01ce0a318e9f36bd4cbdd932c28b3
samssi <samir.hamadi@gmail.com>
2019-09-25 00:47:52 +0300

    Add columns for different set of numbers required for debug

--- src/app.js
+++ src/app.js
@@ -24,13 +24,46 @@ const objectColors = {
     9: "lightblue"
 }
 
-const setCellColor = (cellValue) => {
+const tableColumn = (content) => {
+    const col = document.createElement("div");
+    col.innerHTML = content || "&nbsp;";
+    col.className = "column";
+    return col;
+}
+
+const tableRow = (contentLeft, contentMiddle, contentRight) => {
+    const row = document.createElement("div");
+    row.className = "row";
+
+    row.appendChild(tableColumn(contentLeft));
+    row.appendChild(tableColumn(contentMiddle));
+    row.appendChild(tableColumn(contentRight));
+    
+    return row;
+}
+
+// f = g + h
+// g = distance from starting point
+// h = distance from end point
+const pointCell = () => {
+    const div = document.createElement("div");
+    div.appendChild(tableRow("15", "10", "15"));
+    div.appendChild(tableRow("10", "total", "10"));
+    div.appendChild(tableRow("15", "10", "15"));
+    return div;
+} 
+
+const setCellColor = (td, cellValue) => {
+    if (cellValue == 9) {
+        
+        td.appendChild(pointCell());
+    }
     return objectColors[cellValue];
 }
 
 const createDataElement = (item) => {
     const td = document.createElement("td");
-    td.bgColor = setCellColor(item);
+    td.bgColor = setCellColor(td, item);
     return td;
 }
 
