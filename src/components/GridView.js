cf43afd8d15b91a72fe66566f7d8609e8db44eaa
samssi <samir.hamadi@gmail.com>
2019-10-05 17:39:21 +0300

    Add unique key for cells

--- src/components/GridView.js
+++ src/components/GridView.js
@@ -10,17 +10,18 @@ class GridView extends React.Component {
     }
     
     renderTd(item, x, y) {
-        const cell = <Cell item={item} x={x} y={y}/>;
+        const key = `${x},${y}`
+        const cell = <Cell key={key} item={item} x={x} y={y}/>;
         return cell;
     }
 
     renderTr(objects, y) {
-        const tds = mapIndexed((val, idx) => this.renderTd(val, idx, y), objects);
+        const tds = mapIndexed((val, x) => this.renderTd(val, x, y), objects);
         return <tr>{tds}</tr>;
     }
 
     renderTable() {
-        const tr = mapIndexed((val, idx) => this.renderTr(val, idx), this.props.table.table)
+        const tr = mapIndexed((val, y) => this.renderTr(val, y), this.props.table.table)
         return <table><tbody>{tr}</tbody></table>
     }
 
