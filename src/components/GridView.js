ab1d7b37919bc266e7f74337c169056f47980aa3
samssi <samir.hamadi@gmail.com>
2019-10-05 17:28:19 +0300

    Add x value to cell

--- src/components/GridView.js
+++ src/components/GridView.js
@@ -2,26 +2,24 @@ import React from 'react';
 import * as R from 'ramda';
 import Cell from './Cell';
 
+const mapIndexed = R.addIndex(R.map)
+
 class GridView extends React.Component {
     constructor(props, context) {
         super(props, context);
     }
     
     renderTd(item, x, y) {
-        console.log("item: " + item);
-        console.log("x: " + x);
-        console.log("y: " + y);
         const cell = <Cell item={item} x={x} y={y}/>;
         return cell;
     }
 
     renderTr(objects, y) {
-        const tds = R.map(cells => this.renderTd(cells, 0, y), objects);
+        const tds = mapIndexed((val, idx) => this.renderTd(val, idx, y), objects);
         return <tr>{tds}</tr>;
     }
 
     renderTable() {
-        const mapIndexed = R.addIndex(R.map)
         const tr = mapIndexed((val, idx) => this.renderTr(val, idx), this.props.table.table)
         return <table><tbody>{tr}</tbody></table>
     }
