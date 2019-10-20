a315eefe98bc2831a3c441f0985f6b47a5f15a8b
samssi <samir.hamadi@gmail.com>
2019-10-21 00:17:03 +0300

    Add initial tail recursive H cost calculator for south paths only

--- src/algorithm/astar.js
+++ src/algorithm/astar.js
@@ -13,7 +13,6 @@ export const searchTable = (table, objectType) => {
 };
 
 const searchStartAndEndPositionsFromTable = (table) => {
-  console.log("init start");
     const newStartPosition = { 
       startPosition: searchTable(table, START), 
       endPosition: searchTable(table, END)
@@ -21,15 +20,45 @@ const searchStartAndEndPositionsFromTable = (table) => {
     return newStartPosition;
 }
 
-export const nextStep = (state) => {
-  if (state.stepState === stepState.INIT) {
-    const positions = searchStartAndEndPositionsFromTable(state.table)
-    return {
-      stepInfo: `Found start position: ${positions.startPosition}, end position: ${positions.endPosition}`,
-      stepState: stepState.H_COST,
-      ...positions
-    };
-  }
+const returnStartEndPositions = (state) => {
+  const positions = searchStartAndEndPositionsFromTable(state.table)
+  return {
+    stepInfo: `Found start position: ${positions.startPosition}, end position: ${positions.endPosition}`,
+    stepState: stepState.H_COST,
+    currentPosition: positions.startPosition,
+    ...positions
+  };
+}
 
-  return { }
+const moveSouth = (position) => {
+  const newPosition = R.clone(position);
+  newPosition[0][1] = newPosition[0][1] + 1;
+  return newPosition;
+}
+
+const calculateHCost = (state) => {
+  const currentPosition = R.clone(state.startPosition);
+  const endPosition = R.clone(state.endPosition);
+
+  console.log(endPosition);
+  
+  const recurseToEnd = (cost, position, endPosition) => {
+    if (R.equals(position, endPosition)) return cost;
+    return recurseToEnd(cost + 10, moveSouth(position), endPosition); 
+  }
+  
+  const cost = recurseToEnd(0, currentPosition, endPosition);
+  
+  return {
+    stepInfo: `Calculated H cost of ${cost} for position: ${state.currentPosition}` 
+  }
+}
+
+export const nextStep = (state) => {
+  switch (state.stepState) {
+    case stepState.INIT:
+      return returnStartEndPositions(state);
+    case stepState.H_COST:
+      return calculateHCost(state)
+  }  
 };
--- src/redux/reducers/table.js
+++ src/redux/reducers/table.js
@@ -3,31 +3,29 @@ import * as objectTypes from "../objectTypes";
 import * as modes from "../modes";
 import * as astar from "../../algorithm/astar"
 import * as stepState from "../stepState"
+import * as R from "ramda";
 
 const initialState = {
     table: [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
             [0, 0, 8, 0, 0, 0, 0, 0, 0, 0, 0, 0],
             [0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
             [0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 9, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 9, 0, 0, 0, 0, 0, 0, 0, 0, 0],
             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]],
     mode: modes.RUN,
     editObjectType: objectTypes.FREE.value,
     stepState: stepState.INIT,
     stepInfo: "Astar ready!",
-    startPosition: [-1, -1],
-    endPosition: [-1, -1],
-    currentPosition: {
-      x: -1,
-      y: -1
-    }
+    startPosition: [-1,-1],
+    endPosition: [-1,-1],
+    currentPosition: [-1,-1]
 };
 
 const table = (state = initialState, action) => {
     switch (action.type) {
         case SELECT_CELL:
-            const newTable = [...state.table];
+            const newTable = R.clone(state.table);
             newTable[action.payload.y][action.payload.x] = state.editObjectType;
             return {
                 ...state,
@@ -41,6 +39,7 @@ const table = (state = initialState, action) => {
         case TOGGLE_MODE:
             return {
                 ...state,
+                stepState: stepState.INIT,
                 mode: action.payload.mode
             };
       case NEXT_STEP:
