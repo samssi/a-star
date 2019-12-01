6d4387b6febdbd0d8bfa700fef8526ffdf74fd4f
samssi <samir.hamadi@gmail.com>
2019-12-01 21:08:56 +0200

    Add initial g cost calculation

--- src/algorithm/fghCalculator.js
+++ src/algorithm/fghCalculator.js
@@ -4,24 +4,27 @@ import {SURROUNDING} from "../redux/objectTypes";
 import {hPath} from "./hCalculator";
 import {FGH_COST_LOWEST} from "../redux/stepState";
 
-const drawToTable = (table, cell, endPosition) => {
+const drawToTable = (table, cell, startPosition, endPosition) => {
     const hPathCost = hPath(table, cell, endPosition).totalPathCost;
-    mutateCell(table, cell[0], cell[1], SURROUNDING(-1, hPathCost, -1));
+    // TODO: using heuristic calculation is temp. Heuristics doesn't calculate around objects but direct path through them!
+    const gPathCost = hPath(table, cell, startPosition).totalPathCost;
+    mutateCell(table, cell[0], cell[1], SURROUNDING(gPathCost, hPathCost, -1));
 };
 
-const calculateFgh = (table, cells, endPosition) => {
+const calculateFgh = (table, cells, startPosition, endPosition) => {
     R.forEach(cell => {
-      drawToTable(table, cell, endPosition);
+      drawToTable(table, cell, startPosition, endPosition);
     }, cells);
     return table;
 };
 
 export const calculateFghCosts = (state) => {
     const table = R.clone(state.table);
-    const cells = surroundingCells(state.currentPosition);
+    const cells = surroundingCells(table, state.currentPosition);
+    console.log(cells);
 
     return {
-        table: calculateFgh(table, cells, state.endPosition),
+        table: calculateFgh(table, cells, state.startPosition, state.endPosition),
         stepInfo: "Calculated FGH for next neighboring cells. Results visible",
         stepState: FGH_COST_LOWEST
     }
